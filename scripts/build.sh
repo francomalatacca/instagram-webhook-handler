2b5202d9352a47794b2bdea0626dec3eff1f6b76
francomalatacca <francomalatacca@gmail.com>
2025-04-10 22:34:10 +0200

    update build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 set -e
 
+# Install Python (if needed)
+if ! command -v python3 &> /dev/null; then
+  echo "Installing Python..."
+  apt-get update && apt-get install -y python3 python3-pip
+fi
+
+# Use pip3 instead of pip (more portable)
 echo "Installing AWS SAM CLI..."
-pip install aws-sam-cli
+pip3 install aws-sam-cli
+
+# Confirm install
+echo "SAM CLI version:"
+sam --version
 
 echo "Building the SAM application..."
 sam build
